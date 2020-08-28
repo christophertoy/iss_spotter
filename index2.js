94867d72ed6bd35e553c821f5350a52b95cfd6b2
Christopher <christopherstoy@gmail.com>
2020-08-28 03:42:07 +0000

    updated using promises

---
+++ index2.js
@@ -0,0 +1,27 @@
+// const { fetchMyIP } = require('./iss_promised');
+// const { fetchCoordsByIP } = require('./iss_promised');
+// const { fetchISSFlyOverTimes } = require('./iss_promised');
+const { nextISSTimesForMyLocation } = require('./iss_promised');
+
+// fetchMyIP()
+//   .then(fetchCoordsByIP)
+//   .then(fetchISSFlyOverTimes)
+//   .then(body => console.log(body));
+
+
+const printPassTimes = function(passTimes) {
+  for (const pass of passTimes) {
+    const date = new Date(0);
+    date.setUTCSeconds(pass.risetime);
+    const duration = pass.duration;
+    console.log(`Next pass at ${date} for ${duration} seconds!`);
+  }
+}
+
+nextISSTimesForMyLocation()
+  .then((passTimes) => {
+    printPassTimes(passTimes);
+  })
+  .catch((error) =>{
+    console.log("It didn't work:", error.message);
+  });
